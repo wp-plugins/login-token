54be8d616b87c9be4e16728da6fe3d85d2e4978d
leo108 <leo108@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-15 09:42:07 +0000

    git-svn-id: https://plugins.svn.wordpress.org/login-token/trunk@558421 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 1.0/index.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @package login_token
+ * @version 1.0
+ */
+/*
+Plugin Name: login_token
+Plugin URI: http://wordpress.org/extend/plugins/login-token/
+Description: add a hidden filed values token in login form to avoid brute force attack。在后台登录页面添加了一个隐藏的令牌，用来防止暴力破解。
+Author: leo108
+Version: 1.0
+Author URI: http://leo108.com/
+*/
+session_start();
+function login_token() { 
+	$_SESSION['login_token'] = md5(rand());
+	echo "<input type='hidden' name='login_token' value='{$_SESSION['login_token']}' />";
+}
+add_action('login_form', 'login_token'); 
+if ( !function_exists('wp_authenticate') ) :
+function wp_authenticate($username, $password) {
+	$username = sanitize_user($username);
+	$password = trim($password);
+
+	$user = apply_filters('authenticate', null, $username, $password);
+	if($_POST['login_token'] != $_SESSION['login_token'] && $username != '')
+	{
+		$user = new WP_Error('authentication_failed', 'Login Token Error!');
+	}
+	if ( $user == null ) {
+		$user = new WP_Error('authentication_failed', __('<strong>ERROR</strong>: Invalid username or incorrect password.'));
+	}
+
+	$ignore_codes = array('empty_username', 'empty_password');
+
+	if (is_wp_error($user) && !in_array($user->get_error_code(), $ignore_codes) ) {
+		do_action('wp_login_failed', $username);
+	}
+	return $user;
+}
+endif;
+?>
